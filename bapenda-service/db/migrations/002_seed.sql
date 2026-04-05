214a8158f5ae07b4662c06f45aa499fed008891f
Alfaril Dzaky Praptana <alfarildzaky@gmail.com>
2026-04-05 14:45:30 +0700

    feat: add seed data for tarif_pkb table

---
+++ bapenda-service/db/migrations/002_seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO tarif_pkb (jenis_plat, label, tarif_pkb_persen, opsen_pkb_persen, bbn1_persen, opsen_bbn1_persen, bbn2_persen)
+VALUES
+    ('hitam', 'Umum/Pribadi',  0.0150, 0.6600, 0.1200, 0.6600, 0.0100),
+    ('kuning','Angkutan Umum', 0.0075, 0.6600, 0.0600, 0.6600, 0.0075),
+    ('merah', 'Pemerintah',    0.0075, 0.6600, 0.0000, 0.0000, 0.0000);
