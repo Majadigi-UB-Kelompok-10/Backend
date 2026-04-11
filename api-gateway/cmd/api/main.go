bfaab6e9f93929942815d7e08bfc9aca52fbb0b6
Olfat Faiz <code.olfatfaiz@gmail.com>
2026-04-11 20:08:20 +0700

    - Working Dynamic Endpoint for Proof of Concept + Docker Setup for Dev

--- api-gateway/cmd/api/main.go
+++ api-gateway/cmd/api/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/Majadigi-UB-Kelompok-10/api-gateway/internal/db"
+	"github.com/Majadigi-UB-Kelompok-10/api-gateway/internal/routes"
 	init_helper "github.com/Majadigi-UB-Kelompok-10/majadigi-go-shared/shared/init_helper"
 	"github.com/bytedance/sonic"
 	"github.com/gofiber/fiber/v3"
@@ -21,6 +24,12 @@ func main() {
 	// Initialize Redis cache
 	init_helper.InitializeRedisCache(os.Getenv("REDIS_URL"))
 
+	// Get Port
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8888"
+	}
+
 	// Initialize Fiber app
 	app := fiber.New(fiber.Config{
 		AppName:     "Majadigi Api Gateway",
@@ -31,20 +40,25 @@ func main() {
 	// Initialize Fiber app configuration
 	init_helper.InitializeFiberAppConfig(app, os.Getenv("ALLOWED_ORIGINS"))
 
+	// Setting Up
+	queries := db.New(pool)
+	routes.SetupDynamicEndpoint(app, queries, context.Background())
+	fmt.Println("Dynamic endpoints configured")
+
+	// Listen to port
+	go func() {
+		if err := app.Listen(":" + port); err != nil && err.Error() != "shutting down" {
+			log.Printf("Api Gateway error: %v\n", err)
+		}
+
+		fmt.Println("Api Gateway listening on :" + port)
+	}()
+
 	// Initialize graceful shutdown listener for Fiber App, PostgreSQL, and Redis (Implicit)
 	init_helper.InitializeGracefulShutdownListener(&init_helper.ShutdownType{App: app, Pool: pool})
 
 	// Initialize graceful shutdown listener for other specific handler, worker, or services
 	gracefulShutdownListener()
-
-	// Listen to port 8080
-	go func() {
-		if err := app.Listen(":8080"); err != nil && err.Error() != "shutting down" {
-			log.Printf("Server error: %v\n", err)
-		}
-	}()
-
-	fmt.Println("Server listening on :8080")
 }
 
 func gracefulShutdownListener() {
