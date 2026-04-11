6ed6b4618f201da82c6fd6bafaced5860486d890
Olfat Faiz <code.olfatfaiz@gmail.com>
2026-04-11 20:26:53 +0700

    - Fix Seed - Add proxy - Fix Network in compose

--- api-gateway/internal/routes/api.go
+++ api-gateway/internal/routes/api.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 
 	"github.com/Majadigi-UB-Kelompok-10/api-gateway/internal/db"
-	"github.com/Majadigi-UB-Kelompok-10/api-gateway/internal/handlers"
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/proxy"
 )
 
 func SetupDynamicEndpoint(app *fiber.App, queries *db.Queries, ctx context.Context) error {
@@ -20,21 +20,29 @@ func SetupDynamicEndpoint(app *fiber.App, queries *db.Queries, ctx context.Conte
 	// Generate based on Data
 	for _, route := range routes {
 		slug := route.SlugName
-		id := route.EndpointListID
 		url := route.PageUrl
 
 		// Register the GET endpoint using the slug_name
 		api.Get("/"+slug, func(c fiber.Ctx) error {
 
 			// For now, just return a success message acknowledging the slug
-			return c.JSON(handlers.SuccessResponse{
-				Pesan: "Sukses",
-				Data: map[string]any{
-					"slug": slug,
-					"id":   id,
-					"url":  url,
-				},
-			})
+			// return c.JSON(handlers.SuccessResponse{
+			// 	Pesan: "Sukses",
+			// 	Data: map[string]any{
+			// 		"slug": slug,
+			// 		"id":   id,
+			// 		"url":  url,
+			// 	},
+			// })
+
+			if err := proxy.Do(c, url); err != nil {
+				fmt.Printf("Proxy error for slug %s to %s: %v\n", slug, url, err)
+				return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{
+					"error": "Service temporarily unavailable",
+				})
+			}
+
+			return nil
 		})
 	}
 
