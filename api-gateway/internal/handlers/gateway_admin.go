1b18e10468bb5aabd9ae4ff5b1876ffc5315da94
Alfaril Dzaky Praptana <alfarildzaky@gmail.com>
2026-04-19 11:17:58 +0700

    Feat: Implement GatewayAdminHandler with SyncNow function for manual endpoint synchronization

---
+++ api-gateway/internal/handlers/gateway_admin.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"context"
+	"time"
+
+	"github.com/Majadigi-UB-Kelompok-10/api-gateway/internal/db"
+	"github.com/Majadigi-UB-Kelompok-10/api-gateway/internal/routes"
+	"github.com/gofiber/fiber/v3"
+)
+
+type GatewayAdminHandler struct {
+	Queries *db.Queries
+}
+
+func NewGatewayAdminHandler(q *db.Queries) *GatewayAdminHandler {
+	return &GatewayAdminHandler{Queries: q}
+}
+
+func (h *GatewayAdminHandler) SyncNow(c fiber.Ctx) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := routes.RefreshEndpoints(ctx, h.Queries); err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"error": "Gagal sinkron",
+			"pesan": err.Error(),
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"message": "Gateway berhasil disinkronkan secara manual!",
+	})
+}
