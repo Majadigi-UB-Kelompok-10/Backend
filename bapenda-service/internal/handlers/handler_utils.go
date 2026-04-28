2b85ba6ce49e2c2a4c18aea3280aa0a13978ac02
Alfaril Dzaky Praptana <alfarildzaky@gmail.com>
2026-04-29 00:29:22 +0700

    Feat: Add handler utility functions for environment-based cache TTL configuration

---
+++ bapenda-service/internal/handlers/handler_utils.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"os"
+	"time"
+)
+
+func getEnvDuration(key string, defaultVal time.Duration) time.Duration {
+	val := os.Getenv(key)
+	if val == "" {
+		return defaultVal
+	}
+	parsed, err := time.ParseDuration(val)
+	if err != nil {
+		return defaultVal
+	}
+	return parsed
+}
+
+// Variabel Global untuk TTL (Bisa diatur dari .env)
+var (
+	CacheTTLList   = getEnvDuration("CACHE_TTL_LIST", 5*time.Minute)
+	CacheTTLDetail = getEnvDuration("CACHE_TTL_DETAIL", 30*time.Minute)
+	CacheTTLMaps   = getEnvDuration("CACHE_TTL_MAPS", 1*time.Hour)  
+	CacheTTLStatic = getEnvDuration("CACHE_TTL_STATIC", 24*time.Hour) 
+)
