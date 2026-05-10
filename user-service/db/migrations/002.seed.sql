b8ef44e321fc7710bbc9efd0bda9a2b33b61d778
Alfaril Dzaky Praptana <alfarildzaky@gmail.com>
2026-05-10 20:48:26 +0700

    feat(seed): add seed data for default superadmin account in database

---
+++ user-service/db/migrations/002.seed.sql
@@ -0,0 +1,14 @@
+-- Default superadmin account (password: Admin@12345)
+-- Hash generated via: bcrypt.GenerateFromPassword([]byte("Admin@12345"), 12)
+INSERT INTO users (
+    first_name, last_name, phone, email, nik, address,
+    password_hash, role
+) VALUES (
+    'Super', 'Admin',
+    '+6281234567890',
+    'superadmin@majadigi.id',
+    '1234567890123456',
+    'Surabaya, Jawa Timur',
+    '$2a$12$VPDgCGQvvoBdvX0krzrlU.W09r2qPiuUbORXASlWNtGFr/GJ9hcya',
+    'superadmin'
+) ON CONFLICT DO NOTHING;
