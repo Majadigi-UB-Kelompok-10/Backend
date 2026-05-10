01dd6526e36f286f3db82c0da307368b02b878dd
Alfaril Dzaky Praptana <alfarildzaky@gmail.com>
2026-05-10 20:30:08 +0700

    feat(handlers): add ParseUUID function for UUID parsing and conversion

---
+++ api-gateway/internal/handlers/util.go
@@ -0,0 +1,14 @@
+package handlers
+
+import (
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+func ParseUUID(s string) (pgtype.UUID, error) {
+	id, err := uuid.Parse(s)
+	if err != nil {
+		return pgtype.UUID{}, err
+	}
+	return pgtype.UUID{Bytes: id, Valid: true}, nil
+}
